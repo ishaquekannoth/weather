2e450a20d2642965614a65677aef0bf9f44caa69
Ishaque <ishaque.k@buddyloan.com>
2025-09-06 18:08:17 +0530

    button modification and finalisation

--- lib/features/weather/presentation/pages/weather_page.dart
+++ lib/features/weather/presentation/pages/weather_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:weather/core/themes/colors.dart';
 import 'package:weather/features/weather/presentation/bloc/weather_bloc.dart';
 import 'package:weather/features/weather/presentation/widgets/weather_loading.dart';
 import 'package:weather/features/weather/presentation/widgets/weather_widgets.dart';
@@ -49,27 +50,71 @@ class _WeatherPageState extends State<WeatherPage> {
               const SizedBox(height: 12),
               SizedBox(
                 width: double.infinity,
-                height: 48.h,
-                child: ElevatedButton(
-                  onPressed: () {
-                    final city = _controller.text.trim();
-                    if (city.isNotEmpty) {
-                      context.read<WeatherBloc>().add(FetchWeather(city));
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+                height: 55.h,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.r),
+                    gradient: const LinearGradient(
+                      colors: [
+                        AppColors.sapphireBlue,
+                        AppColors.darkPurple,
+                      ], // Charming gradient
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
                     ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.green.withOpacity(0.4), // Soft shadow
+                        spreadRadius: 2,
+                        blurRadius: 8,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
                   ),
-                  child: const Text(
-                    "Get Weather",
-                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final city = _controller.text.trim();
+                      if (city.isNotEmpty) {
+                        context.read<WeatherBloc>().add(FetchWeather(city));
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors
+                          .transparent, // Make button background transparent
+                      shadowColor: Colors.transparent, // Remove default shadow
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 15,
+                      ),
+                      elevation: 0,
+                    ),
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.wb_cloudy_outlined,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Get Weather",
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 0.8,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
+              SizedBox(height: 20.h),
 
               Expanded(
                 child: Center(
@@ -107,11 +152,11 @@ class _WeatherPageState extends State<WeatherPage> {
         mainAxisSize: MainAxisSize.min,
         children: [
           const Icon(Icons.error, color: Colors.red),
-          const SizedBox(width: 8),
+          SizedBox(width: 8.w),
           Flexible(
             child: Text(
               message,
-              style: const TextStyle(color: Colors.red, fontSize: 14),
+              style: TextStyle(color: Colors.red, fontSize: 14.h),
             ),
           ),
         ],
